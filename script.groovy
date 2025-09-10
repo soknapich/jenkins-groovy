a41718f14ea98d4177e1a7ba5c1e5c0f2be98d11
Sokna Pich <soknait@gmail.com>
2025-09-10 09:41:56 +0700

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -13,6 +13,12 @@ def deployCompose() {
     sh """
         docker rm -f nginx 2>/dev/null || true && docker run -d --name nginx -p 80:80 nginx:latest
     """
+
+     sshagent(['domrey']) {
+        sh """
+        scp -o StrictHostKeyChecking=no Jenkinsfile sokna@${EC2_IP}:/home/sokna
+        """
+    }
     
 }
 
