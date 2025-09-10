b3419ac485d4a5294c09b6763b5cc0bff94f6faa
Sokna Pich <soknait@gmail.com>
2025-09-10 10:30:57 +0700

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -16,21 +16,21 @@ def deployCompose() {
 }
 
 def sshToDomrey(){
-    // withCredentials([usernamePassword(credentialsId: 'my-ssh-server', 
-    //                                  usernameVariable: 'SSH_USER', 
-    //                                  passwordVariable: 'SSH_PASS')]) {
-    //     sh '''
-    //         sshpass -p "$SSH_PASS" ssh -o StrictHostKeyChecking=no $SSH_USER@domrey.com "hostname && whoami"
-    //         sshpass -p "$SSH_PASS" scp -o StrictHostKeyChecking=no script.groovy $SSH_USER@domrey.com:/home/$SSH_USER/
-    //     '''
-    // }
+    withCredentials([usernamePassword(credentialsId: 'my-ssh-server', 
+                                     usernameVariable: 'SSH_USER', 
+                                     passwordVariable: 'SSH_PASS')]) {
+        sh '''
+            sshpass -p "$SSH_PASS" ssh -o StrictHostKeyChecking=no $SSH_USER@domrey.com "hostname && whoami"
+            sshpass -p "$SSH_PASS" scp -o StrictHostKeyChecking=no script.groovy $SSH_USER@domrey.com:/home/$SSH_USER/
+        '''
+    }
 
-    sshagent(['ssh-ec2']) {
-    sh '''
-        ssh -o StrictHostKeyChecking=no ubuntu@54.255.232.240 "hostname && whoami" 
-        scp -o StrictHostKeyChecking=no script.groovy ubuntu@54.255.232.240:/home/ubuntu
-    '''
-}
+    // sshagent(['ssh-ec2']) {
+    // sh '''
+    //     ssh -o StrictHostKeyChecking=no ubuntu@54.255.232.240 "hostname && whoami" 
+    //     scp -o StrictHostKeyChecking=no script.groovy ubuntu@54.255.232.240:/home/ubuntu
+    // '''
+    // }
 
 }
 
