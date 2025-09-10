9b6e81d690eeaf81c2a0d7b41fdb9c0f769a7311
Sokna Pich <soknait@gmail.com>
2025-09-10 09:54:06 +0700

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -12,14 +12,18 @@ def deployCompose() {
     echo "Deploying with Docker Compose..."
     sh """
         docker rm -f nginx 2>/dev/null || true && docker run -d --name nginx -p 80:80 nginx:latest
-    """
+    """    
+}
 
-     sshagent(['domrey']) {
-        sh """
-        scp -o StrictHostKeyChecking=no Jenkinsfile sokna@${EC2_IP}:/home/sokna
-        """
+def sshToDomrey(){
+    withCredentials([usernamePassword(credentialsId: 'my-ssh-server', 
+                                     usernameVariable: 'SSH_USER', 
+                                     passwordVariable: 'SSH_PASS')]) {
+        sh '''
+            sshpass -p "$SSH_PASS" ssh -o StrictHostKeyChecking=no $SSH_USER@ydomrey.com "hostname && whoami"
+            sshpass -p "$SSH_PASS" scp -o StrictHostKeyChecking=no scrip.groovy $SSH_USER@domrey.com:/home/$SSH_USER/
+        '''
     }
-    
 }
 
 return this
